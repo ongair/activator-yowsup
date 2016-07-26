102cc40c801eddcb5101c4fe957dd5ae2b31e0e8
Trevor Kimenye <kimenye@gmail.com>
2016-07-26 12:22:17 +0300

    Returning identity

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+
+from yowsup.common.tools import StorageTools, WATools
+from yowsup.registration.existsrequest import WAExistsRequest
+from yowsup.env import S40YowsupEnv, AndroidYowsupEnv
+
 import random, hashlib, os
+from urllib import quote
+CURRENT_ENV = AndroidYowsupEnv()
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET'])
+def index():
+  return jsonify(status=200)
+
 @app.route("/token", methods=['GET'])
-def req():
+def token():
   number = request.args.get('number')
   cc = request.args.get('cc')
 
+
   if number is not None:
     if cc is not None:
-      # 
-      return jsonify(status=200, number=number, cc=cc)
+
+      in_ = number[len(cc):]
+      token = CURRENT_ENV.getToken(in_)
+      idx = WATools.generateIdentity()
+
+      return jsonify(status=200, number=number, cc=cc, token=token, identity=quote(idx))
     else:
       return jsonify(status=402, error="Country code is required")
   else:
